a1d096da6d7f64898e4ad1ca120ccd0b5acf8fb2
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-08-09 00:38:02 -0300

    update form
    Atualizando as animações do formulário!

---
+++ src/js/form.js
@@ -0,0 +1,12 @@
+// Variáveis
+var formLogin = document.getElementById('form-login')
+var formCad = document.getElementById('form-cad')
+
+function switchFormCad() {
+    formLogin.classList.replace('show-form', 'close-form')
+    
+}
+
+function switchFormLogin() {
+
+}
