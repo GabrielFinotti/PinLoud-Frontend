5fb9767e9db47422d5e96b14b7f2583d182a7a3a
Júlia Lucena <96270130+Sasacc@users.noreply.github.com>
2023-08-11 23:08:19 -0300

    Validate Cadastro e Login

--- src/js/form.js
+++ src/js/form.js
@@ -2,6 +2,13 @@
 var formLogin = document.getElementById('form-login')
 var formCad = document.getElementById('form-cad')
 var mainForm = document.getElementById('main-form')
+var errorMessage = document.getElementById('error-message');
+var successMessage = document.getElementById('success-message');
+var cadUsername = document.getElementById('cad-user');
+var cadEmail = document.getElementById('cad-email');
+var cadPassword = document.getElementById('cad-password');
+var cadPasswordConfirm = document.getElementById('cad-password-confirm');
+
 
 function switchFormCad() {
     formLogin.classList.replace('show-form', 'close-form')
@@ -27,3 +34,69 @@ function switchFormCad() {
     formLogin.classList.replace('close-form', 'show-form')
 }
 // Função para as verificações dos inputs no front-end!
+
+function validateCadastro() {
+    var username = cadUsername.value;
+    var email = cadEmail.value;
+    var password = cadPassword.value;
+    var confirmPassword = cadPasswordConfirm.value;
+
+    if (username === "" || email === "" || password === "" || confirmPassword === "") {
+        errorMessage.textContent = "Por favor, preencha todos os campos.";
+        return;
+    }
+
+    if (password.length < 8) {
+        errorMessage.textContent = "A senha deve ter pelo menos 8 caracteres.";
+        return;
+    }
+    if (!email.includes("@") || !email.includes(".")) {
+        errorMessage.textContent = "Por favor, insira um endereço de e-mail válido.";
+        return;
+    }
+    if (password !== confirmPassword) {
+        errorMessage.textContent = "As senhas não coincidem. Por favor, digite novamente.";
+        return;
+    }
+
+    // Se todas as verificações estiverem ok
+    errorMessage.textContent = ""; 
+    successMessage.textContent = "Cadastro Realizado com sucesso";
+
+    cadUsername.value = "";
+    cadEmail.value = "";
+    cadPassword.value = "";
+    cadPasswordConfirm.value = "";
+
+    // Voltar para o formulário de login 
+    setTimeout(() => {
+        successMessage.textContent = ""; 
+        switchFormLogin();
+    }, 300); 
+}
+
+//Validação login
+var loginErrorMessage = document.getElementById('login-error-message');
+var loginEmail = document.getElementById('login-email');
+var loginPassword = document.getElementById('login-password');
+
+function validateLogin() {
+    var email = loginEmail.value;
+    var password = loginPassword.value;
+
+    if (!email.includes("@") || !email.includes(".")) {
+        loginErrorMessage.textContent = "Por favor, insira um e-mail válido.";
+        return;
+    }
+
+    if (password.length < 8) {
+        loginErrorMessage.textContent = "A senha e o e-mail deve coincidir.";
+        return;
+    }
+
+    // Se todas as verificações estiverem ok
+    loginErrorMessage.textContent = ""; 
+
+    // Redirecionar para a Home
+    window.location.href = "../../index.html"; 
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,29 @@
-// Pré-visualização
-document.addEventListener("DOMContentLoaded", function () {
-    var uploadInput = document.getElementById("uploadInput");
-    var previewImage = document.getElementById("previewImage");
+//Prévia imagem
+document.addEventListener('DOMContentLoaded', function () {
+    var uploadInput = document.getElementById('uploadInput');
+    var previewImage = document.getElementById('previewImage');
+    var errorMessage = document.getElementById('errorMessage');
 
-    uploadInput.addEventListener("change", function (event) {
+    uploadInput.addEventListener('change', function (event) {
         var file = event.target.files[0];
 
-        if (file) {
-            var validExtensions = ["jpg", "jpeg", "png", "gif"];
-            var fileExtension = file.name.split(".").pop().toLowerCase();
-
-            if (validExtensions.includes(fileExtension)) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    previewImage.src = e.target.result;
-                    previewImage.style.display = "block";
-                };
-
-                reader.readAsDataURL(file);
-            } else {
-                alert("Por favor, escolha um arquivo com extensão jpg, jpeg, png ou gif.");
-                uploadInput.value = ""; // Limpar a seleção do arquivo inválido
-                previewImage.style.display = "none"; // Esconder a imagem de pré-visualização
-            }
+        var allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+        var fileExtension = file.name.toLowerCase().substring(file.name.lastIndexOf('.'));
+        if (allowedExtensions.indexOf(fileExtension) === -1) {
+            errorMessage.textContent = 'Por favor, escolha um arquivo com extensão jpg, jpeg, png ou gif.';
+            previewImage.style.display = 'none';
+            return;
         }
+
+        errorMessage.textContent = '';
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            previewImage.src = e.target.result;
+            previewImage.style.display = 'block';
+        };
+
+        reader.readAsDataURL(file);
     });
 });
