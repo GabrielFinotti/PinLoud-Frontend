e036af418313e3711ec73eda3f0fb06cfbcf9466
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-06 10:48:23 -0300

    Update UI/UX
    Página de formulário, e barra de navegação para telas além do mobile concluída.
    Refatoração e reescrita do código de abertura do menu de navegação para mobile efetuada!

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
   // Variáveis do componente
   protected navItens!: boolean;
+  protected display!: Record<string, string>;
 
   constructor() {
     this.navItens = false;
@@ -20,8 +22,14 @@ export class NavBarComponent {
   public showMenu() {
     if (this.navItens) {
       this.navItens = false;
+      this.display = {
+        display: 'none',
+      };
     } else {
       this.navItens = true;
+      this.display = {
+        display: 'block'
+      };
     }
   }
 }
