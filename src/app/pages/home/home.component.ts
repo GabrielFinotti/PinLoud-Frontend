1416fdcddab345512f1b563df1a6900877a36210
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-06 07:22:54 -0300

    Navbar mobile
    Formatação e construção da barra de navegação para dispositivos mobile concluída!

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  // Variáveis do componente
+  protected navItens!: boolean;
 
+  constructor() {
+    this.navItens = false;
+  }
+
+  // Abrir e fechar a barra de navegação
+  public showMenu() {
+    if (this.navItens) {
+      this.navItens = false;
+    } else {
+      this.navItens = true;
+    }
+  }
 }
