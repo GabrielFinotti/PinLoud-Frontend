bc06d9935b031913e2fd6a90b8ae8806b28a8095
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-04 14:09:54 -0300

    Reset

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'pinload' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pinload');
   });
 
   it('should render title', () => {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,3 @@
 import { Routes } from '@angular/router';
 
-// Componentes
-import { HomeComponent } from './pages/home/home.component';
-import { ExplorarComponent } from './pages/explorar/explorar.component';
-import { CriarComponent } from './pages/criar/criar.component';
-import { UserComponent } from './pages/user/user.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { FormularioComponent } from './pages/formulario/formulario.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'explorar', component: ExplorarComponent },
-  { path: 'criar', component: CriarComponent },
-  {
-    path: 'usuario',
-    children: [{ path: 'formulario', component: FormularioComponent }],
-  },
-  { path: '**', component: NotFoundComponent },
-];
+export const routes: Routes = [];
