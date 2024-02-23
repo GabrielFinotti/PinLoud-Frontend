047456d26775a2f7fbc0b9eb25356275e7d02229
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-23 14:27:59 -0300

    Search Pin
    Atualização da formatação.

--- src/app/pages/pins/pins.component.ts
+++ src/app/pages/pins/pins.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { SearchPinComponent } from '../../shared/components/search-pin/search-pin.component';
 
 @Component({
   selector: 'app-pins',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SearchPinComponent],
   templateUrl: './pins.component.html',
   styleUrl: './pins.component.scss',
 })
