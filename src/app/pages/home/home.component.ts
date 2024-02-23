35699170030995b56d8d42d02ca9c082437972c0
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-23 15:59:28 -0300

    Bug Fixed
    Corrigido o bug do travamento da tela ocasionado pela navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { SearchPinComponent } from '../../shared/components/search-pin/search-pin.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SearchPinComponent],
+  imports: [FooterComponent, SearchPinComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/pages/pins/pins.component.ts
+++ src/app/pages/pins/pins.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { SearchPinComponent } from '../../shared/components/search-pin/search-pin.component';
 
 @Component({
   selector: 'app-pins',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SearchPinComponent],
+  imports: [FooterComponent, SearchPinComponent],
   templateUrl: './pins.component.html',
   styleUrl: './pins.component.scss',
 })
