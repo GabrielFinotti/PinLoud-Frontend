94496e36639465e4b168ba18394c4eb47e49b76e
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-26 16:46:41 -0300

    Register Form
    Estruturação e formatação do formulário de registro completo!

--- src/app/components/forms/register-form/register-form.component.ts
+++ src/app/components/forms/register-form/register-form.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.scss'
+  styleUrl: './register-form.component.scss',
 })
 export class RegisterFormComponent {
+  public registerForm!: FormGroup;
 
+  constructor(formbuilder: FormBuilder) {}
 }
