6c7e813f632b33cd48e5ddef9550eaafe949111f
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-26 16:16:46 -0300

    Refact
    Refatoração e otimização do código.
    Início da construção da página dos formulários de usuários.

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { RegisterFormComponent } from '../../components/forms/register-form/register-form.component';
+import { LoginFormComponent } from '../../components/forms/login-form/login-form.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RegisterFormComponent,
+    LoginFormComponent,
+  ],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
-export class FormComponent {
-
-}
+export class FormComponent {}
