83f17905fab9ec11d87f644c3a1ade0695500e61
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-26 15:31:49 -0300

    Pin Create
    Formulário de criação dos pins para mobile criado!

--- src/app/components/forms/pin-create-form/pin-create-form.component.ts
+++ src/app/components/forms/pin-create-form/pin-create-form.component.ts
@@ -5,8 +5,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './pin-create-form.component.html',
-  styleUrl: './pin-create-form.component.scss'
+  styleUrl: './pin-create-form.component.scss',
 })
 export class PinCreateFormComponent {
+  public imgPreview!: string | ArrayBuffer;
 
+  public showImagePreview(event: Event) {
+    const target = event.target as HTMLInputElement;
+
+    if (target.files && target.files.length > 0) {
+      const file: File = target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        if (e.target) {
+          this.imgPreview = e.target.result as string;
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
--- src/app/pages/pin-create/pin-create.component.ts
+++ src/app/pages/pin-create/pin-create.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { PinCreateFormComponent } from '../../components/forms/pin-create-form/pin-create-form.component';
 
 @Component({
   selector: 'app-pin-create',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, PinCreateFormComponent],
   templateUrl: './pin-create.component.html',
-  styleUrl: './pin-create.component.scss'
+  styleUrl: './pin-create.component.scss',
 })
-export class PinCreateComponent {
-
-}
+export class PinCreateComponent {}
