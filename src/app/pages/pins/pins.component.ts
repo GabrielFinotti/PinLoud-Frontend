bcf791a11e62ad8a3612b162c560afcece063a9c
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-28 14:55:58 -0300

    Updates
    Refatoração de código, e organização dos card de imagens como componentes na página de pins

--- src/app/pages/pins/pins.component.ts
+++ src/app/pages/pins/pins.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { SearchPinComponent } from '../../shared/components/search-pin/search-pin.component';
+import { PinCardComponent } from '../../components/cards/pin-card/pin-card.component';
 
 @Component({
   selector: 'app-pins',
   standalone: true,
-  imports: [FooterComponent, SearchPinComponent],
+  imports: [FooterComponent, SearchPinComponent, PinCardComponent],
   templateUrl: './pins.component.html',
   styleUrl: './pins.component.scss',
 })
